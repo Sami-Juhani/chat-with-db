89fec2076bbe9231b538ca3b689a4fbcc2f0383b
SamiJuhani <sami.paananen@gmail.com>
2025-02-10 20:35:35 +0200

    (fix): Add production url -> .env + CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,21 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
 from .routes.ask import ask_bp
 
+load_dotenv()
+
+ui_url = os.getenv("UI_URL", "http://localhost:3000")
+
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
+    CORS(app, origins=[ui_url], supports_credentials=True)
     app.config.from_object('config.Config')
 
     # Register Blueprints
     app.register_blueprint(ask_bp)
 
     return app
-    
