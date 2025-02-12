f826511fda36ed23f94e99b00cc3737feea22b96
SamiJuhani <sami.paananen@gmail.com>
2025-02-12 12:45:41 +0200

    (files): update deps, update eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,9 @@
+{
+  "extends": "next/core-web-vitals",
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint"],
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  }
+}
